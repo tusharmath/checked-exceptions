47e3d8d7b8788f4baf199db53795776fbe085c47
Tushar Mathur <tusharmath@gmail.com>
2019-04-28 20:54:31 +0530

    test(test-types): add check for usage with typeOf

--- test-types/types.ts
+++ test-types/types.ts
@@ -6,3 +6,8 @@ E.check('E1')
 
 // $ExpectType CheckedExceptionClass<"E1", { id: number; }>
 E.check('E1', (_: {id: number}) => _.id.toString())
+
+const NotImplemented = E.check('NotImplemented')
+
+// $ExpectType CheckedExceptionClass<"NotImplemented", void>[]
+new Array<typeof NotImplemented>()
